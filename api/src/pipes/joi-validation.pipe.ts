4c8a5f987812bcc9e89649228454ec355977b331
chestercharles <chestercarmer@icloud.com>
2019-10-05 11:02:45 -0700

    add joi to validate requests

---
+++ api/src/pipes/joi-validation.pipe.ts
@@ -0,0 +1,20 @@
+import * as Joi from '@hapi/joi';
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+  constructor(private readonly schema: Object) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    const { error } = Joi.validate(value, this.schema);
+    if (error) {
+      throw new BadRequestException(error);
+    }
+    return value;
+  }
+}
